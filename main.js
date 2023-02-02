e7460deef0e62c03be347e0eab7b26b71043ebef
KYaelDLRG <102941170+KYaelDLRG@users.noreply.github.com>
2023-02-01 19:32:30 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+	// Navbar
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+ 
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+ 
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+// Dark Mode
+let darkmode = document.querySelector('#darkmode');
+ 
+darkmode.onclick = () => {
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('active');
+    }else{
+        darkmode.classList.replace('bx-sun','bx-moon');
+        document.body.classList.remove('active');
+    }
+}
+ 
+// Scroll Reveal
+const sr = ScrollReveal ({
+    origin: 'top',
+    distance: '40px',
+    duration: 1000,
+    reset: true
+});
+ 
+// .s-box, .box, son las que pasman el deslizamiento de los elementos pero pueden ir xd
+sr.reveal(`.home-text, .home-img,
+            .about-img, .about-text,
+            .btn, .connect-text,
+            .contact-box,
+            .contact-box address,
+            .menu, .kids, .services,
+            .hours, .s-box, .box,
+            .maps, .maps-box`, {
+    interval: 200
+})
